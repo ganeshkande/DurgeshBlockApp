ed4de4e67c673e6de801f2388f2d12f56600e784
ganeshkande <ganeshkande906@gmail.com>
2022-06-21 19:10:30 +0530

    DUR-12:declare method in userService interface and provide implementation in serviceImpl class

--- src/main/java/com/bikkadIt/blog/Service/UserServiceImpl.java
+++ src/main/java/com/bikkadIt/blog/Service/UserServiceImpl.java
@@ -1,5 +1,77 @@
 package com.bikkadIt.blog.Service;
 
-public class UserServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bikkadIt.blog.Entity.User;
+import com.bikkadIt.blog.Repository.UserRepo;
+import com.bikkadIt.blog.payLods.UserDto;
+@Service
+public class UserServiceImpl implements UserService {
+     @Autowired
+	private UserRepo userRepo;
+	
+	
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		User User = this.dtoToUser(userDto);
+		User savedUser=this.userRepo.save(User);
+		
+		return this.userToDto(savedUser);
+	}
+
+	@Override
+	public UserDto updateUser(UserDto user, Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto getuserById(Integer userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(Integer UserId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private User dtoToUser(UserDto userDto) {
+		
+		User user=new User();
+		user.setId(userDto.getId());
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setPassword(userDto.getPassword());
+		user.setAbout(userDto.getAbout());
+		
+		return user;
+		
+	}
+	
+	public UserDto userToDto(User user) {
+		
+		UserDto userDto=new UserDto();
+		userDto.setId(user.getId());
+		userDto.setName(user.getName());
+		userDto.setEmail(user.getEmail());
+		userDto.setPassword(user.getPassword());
+		userDto.setAbout(user.getAbout());
+		
+		
+		return userDto;
+		
+		
+		
+	}
 }
